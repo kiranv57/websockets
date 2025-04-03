16e3b520b54a88a51ae96cb86cf67127a6936b75
kiran <kpv2108@gmail.com>
2025-04-03 22:57:10 +0530

    Re-add dist/index.js with corrected line endings

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+console.log('WebSocket server started on port 8080');
+let usercount = 0;
+wss.on('connection', (socket) => {
+    console.log('client server connection established');
+    usercount += 1;
+    console.log(`User connected. Total users: ${usercount}`);
+    socket.on('message', (message) => {
+        // Log the raw message
+        console.log('raw message payload from client:', message);
+        console.log('message payload in string', message.toString());
+        console.log(message.toString() === "ping");
+        if (message.toString() === "ping") {
+            socket.send('pong');
+        }
+    });
+    socket.on('error', (error) => {
+        console.error('Socket error:', error);
+    });
+    socket.on('close', () => {
+        usercount -= 1;
+        console.log(`User disconnected. Total users: ${usercount}`);
+    });
+});
