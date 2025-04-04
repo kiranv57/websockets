c666d76824478c31b6399461179b2943b7210131
kiran <kpv2108@gmail.com>
2025-04-04 21:34:01 +0530

    two clients communicating over websocket

--- dist/index.js
+++ dist/index.js
@@ -3,25 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 console.log('WebSocket server started on port 8080');
-let usercount = 0;
+// let allSockets: Set<WebSocket> = new Set();
+let allSockets = [];
 wss.on('connection', (socket) => {
     console.log('client server connection established');
-    usercount += 1;
-    console.log(`User connected. Total users: ${usercount}`);
+    allSockets.push(socket);
     socket.on('message', (message) => {
-        // Log the raw message
-        console.log('raw message payload from client:', message);
-        console.log('message payload in string', message.toString());
-        console.log(message.toString() === "ping");
-        if (message.toString() === "ping") {
-            socket.send('pong');
-        }
+        //    console.log(message.toString());
+        allSockets.filter((s) => (s != socket)).map((ws) => {
+            ws.send(message.toString());
+        });
     });
     socket.on('error', (error) => {
         console.error('Socket error:', error);
     });
     socket.on('close', () => {
-        usercount -= 1;
-        console.log(`User disconnected. Total users: ${usercount}`);
+        console.log('socket connection closed');
+    });
+    socket.on('disconnect', () => {
+        allSockets.filter((s) => s != socket);
+        console.log('User disconnected');
+    });
+    allSockets.forEach((s) => {
+        s.send('you are connected ');
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,41 @@
-import {WebSocketServer} from 'ws'
+import {WebSocketServer, WebSocket} from 'ws'
 
 const wss = new WebSocketServer({port : 8080});
 console.log('WebSocket server started on port 8080');
 
-let usercount = 0;
+
+// let allSockets: Set<WebSocket> = new Set();
+let allSockets: WebSocket[] = [];
 
 wss.on('connection',(socket)=>{
     console.log('client server connection established');
    
-    usercount += 1;
-    console.log(`User connected. Total users: ${usercount}`);
-    
+    allSockets.push(socket);
+
     socket.on('message', (message)=>{
-        // Log the raw message
-        console.log('raw message payload from client:', message);
-        console.log('message payload in string', message.toString())
-        console.log(message.toString() === "ping") 
-        
-        if(message.toString() === "ping"){
-            socket.send('pong');
-        }
+    //    console.log(message.toString());
+
+       allSockets.filter((s) => (s != socket)).map((ws)=>{
+           ws.send(message.toString())
+       })
+
     });
+
     socket.on('error', (error) => {
         console.error('Socket error:', error);
     });
     socket.on('close', () => {
-        usercount -= 1;
-        console.log(`User disconnected. Total users: ${usercount}`);
+        
+        console.log('socket connection closed')
     });
+
+    socket.on('disconnect', () => {
+        allSockets.filter((s)=> s != socket)
+        console.log('User disconnected')
+    });
+
+    allSockets.forEach((s)=>{
+       s.send('you are connected ')
+    })
 });
 
