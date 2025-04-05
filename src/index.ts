6edc7d9f0a7c1b1b37ae6cc02cecc80f65a12fc6
kiran <kpv2108@gmail.com>
2025-04-05 23:46:22 +0530

    formmmated

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,31 @@
-import {WebSocketServer, WebSocket} from 'ws'
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({port : 8081});
-console.log('WebSocket server started on port 8080');
-
+const wss = new WebSocketServer({ port: 8081 });
+console.log("WebSocket server started on port 8080");
 
 // let allSockets: Set<WebSocket> = new Set();
 let allSockets: WebSocket[] = [];
 
 interface roomProps {
-    [key: string] : WebSocket []
+  [key: string]: WebSocket[];
 }
 // room map
-let rooms: roomProps = {} ;
-let room = 'default';
+let rooms: roomProps = {};
+let room = "default";
 
-wss.on('connection',(socket)=>{
-    console.log('client server connection established');
-   
-    allSockets.push(socket);
-    
-    // Get room from URL parameters
-    // const url = new URL(socket.url, 'ws://localhost:8081');
-    // socket.url has path after base url, so to URL, we pass path + base url
-    //  socket.url = ?q=room=chatroom123, base url = ws://localhost:8081, full url: ws://localhost:8081?room=chatroom123
+wss.on("connection", (socket) => {
+  console.log("client server connection established");
 
-    // const room = url.searchParams.get('room') || 'default';
- 
-    
+  allSockets.push(socket);
 
+  // Get room from URL parameters
+  // const url = new URL(socket.url, 'ws://localhost:8081');
+  // socket.url has path after base url, so to URL, we pass path + base url
+  //  socket.url = ?q=room=chatroom123, base url = ws://localhost:8081, full url: ws://localhost:8081?room=chatroom123
 
-    
+  // const room = url.searchParams.get('room') || 'default';
 
-    socket.on('message', (message)=>{
+  socket.on("message", (message) => {
     //    console.log(message.toString());
 
     //    allSockets.filter((s) => (s != socket)).map((ws)=>{
@@ -39,39 +33,38 @@ wss.on('connection',(socket)=>{
     //    })
 
     const msg = JSON.parse(message as unknown as string);
-    console.log('parsed message',msg)
+    console.log("parsed message", msg);
     room = msg.room;
-        if(!rooms[room])
-        {
-            rooms[room] = [];
-        }
-    if(!rooms[room].includes(socket)){
-        rooms[room].push(socket);
+    if (!rooms[room]) {
+      rooms[room] = [];
     }
-    //  console.log('room',rooms)    
-    rooms[room].filter((ws: WebSocket)=> ws != socket).map((ws: WebSocket)=>{
-        ws.send(msg.content)
-    })
+    if (!rooms[room].includes(socket)) {
+      rooms[room].push(socket);
+    }
+    //  console.log('room',rooms)
+    rooms[room]
+      .filter((ws: WebSocket) => ws != socket)
+      .map((ws: WebSocket) => {
+        ws.send(msg.content);
+      });
+  });
 
-    });
+  socket.on("error", (error) => {
+    console.error("Socket error:", error);
+  });
+  socket.on("close", () => {
+    rooms[room] = rooms[room].filter((ws: WebSocket) => ws != socket);
 
-    socket.on('error', (error) => {
-        console.error('Socket error:', error);
-    });
-    socket.on('close', () => {
-        rooms[room] = rooms[room].filter((ws: WebSocket)=> ws != socket)
-       
-        console.log('socket connection closed')
-    });
+    console.log("socket connection closed");
+  });
 
-    socket.on('disconnect', () => {
-        // allSockets.filter((s)=> s != socket)
-         rooms[room] = rooms[room].filter((ws: WebSocket) => ws != socket)
-        console.log('User disconnected')
-    });
+  socket.on("disconnect", () => {
+    // allSockets.filter((s)=> s != socket)
+    rooms[room] = rooms[room].filter((ws: WebSocket) => ws != socket);
+    console.log("User disconnected");
+  });
 
-    // allSockets.forEach((s)=>{
-    //    s.send('you are connected ')
-    // })
+  // allSockets.forEach((s)=>{
+  //    s.send('you are connected ')
+  // })
 });
-
